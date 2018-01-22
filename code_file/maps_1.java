01423b9a67f1244ba649ab033e4b8c78bb2e5da4
kucingapes <kucingapes@gmail.com>
2018-01-22 18:36:21 +0700

    wew

---
+++ code_file/maps_1.java
@@ -0,0 +1,21 @@
+public class Maps_1 extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_1);
+    
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map1);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+}
